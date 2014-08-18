c5248a042bc9fd18f15962824f4969d4852d3e6c
Patrick Wolfert <patrickrwolfert@gmail.com>
2014-08-18 12:26:57 -0700

    Got some nice looking sliders now. I eventually want to fork this slider library, though, and make the move function use css transforms instead of left/top so sliding renders faster.

--- wave-interference/src/js/views/sim-water.js
+++ wave-interference/src/js/views/sim-water.js
@@ -2,12 +2,13 @@ define([
 	'jquery', 
 	'underscore', 
 	'backbone',
+	'nouislider',
 
 	'views/sim',
 	'models/wave-sim-water',
 
 	'text!templates/sim-water.html',
-], function ($, _, Backbone, SimView, WaterSimulation, template) {
+], function ($, _, Backbone, noui, SimView, WaterSimulation, template) {
 
 	'use strict';
 
@@ -38,6 +39,25 @@ define([
 				.off('worse',  $.proxy(this.detachFaucetControls, this))
 				.on( 'worse',  $.proxy(this.detachFaucetControls, this));
 			this.detachFaucetControls();
+
+			// TODO: put this in SimView and just tie it to oscillators
+			this.$('#water-frequency').noUiSlider({
+				start: 0.5,
+				connect: 'lower',
+				range: {
+					min: 0,
+					max: 3
+				}
+			});
+
+			this.$('#water-amplitude').noUiSlider({
+				start: 1.0,
+				connect: 'lower',
+				range: {
+					min: 0,
+					max: 2
+				}
+			});
 		},
 
 		detachFaucetControls: function() {
