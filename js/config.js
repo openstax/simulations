aee6aa464de496b2ba1b398ef316eed954469a29
Patrick Wolfert <patrickrwolfert@gmail.com>
2014-08-27 10:03:03 -0700

    Updates

--- js/config.js
+++ js/config.js
@@ -3,23 +3,23 @@
 
 	var config = {
 		paths: {
-			jquery:     '../../bower_components/jquery/dist/jquery',
-			underscore: '../../bower_components/lodash/dist/lodash',
-			backbone:   '../../bower_components/backbone/backbone',
-			text:       '../../bower_components/requirejs-text/text',
-			pixi:       '../../bower_components/pixi/bin/pixi',
-			nouislider: '../../bower_components/nouislider/jquery.nouislider.full.min',
+			jquery:     '../bower_components/jquery/dist/jquery',
+			underscore: '../bower_components/lodash/dist/lodash',
+			backbone:   '../bower_components/backbone/backbone',
+			text:       '../bower_components/requirejs-text/text',
+			pixi:       '../bower_components/pixi/bin/pixi',
+			nouislider: '../bower_components/nouislider/jquery.nouislider.full.min',
 
 			templates:  '../templates/'
 		},
 
 		packages: [{
 			name: 'css',
-			location: '../../bower_components/require-css',
+			location: '../bower_components/require-css',
 			main: 'css'
 		}, {
 			name: 'less',
-			location: '../../bower_components/require-less',
+			location: '../bower_components/require-less',
 			main: 'less'
 		}],
 	};
