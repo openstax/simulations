4b60da6c4f76f10bf18a8e72b55181d8817e6119
Patrick Wolfert <patrickrwolfert@gmail.com>
2014-09-03 12:12:06 -0700

    Now safe to switch tabs. Updater.js checks to see if it's dormant (requestAnimationFrame being called at less than 3Hz) and pauses itself, forgetting time in between.

--- wave-interference/src/js/utils/updater.js
+++ wave-interference/src/js/utils/updater.js
@@ -11,28 +11,75 @@ define(function (require) {
 
 	var Updater = function() {
 
-		var listeners = [];
+		var DORMANCY_THRESHOLD = 333;      // 3Hz in millseconds
+		var DORMANCY_CHECK_INTERVAL = 100; // ms
+
+		var listeners = {
+			update: [],
+			sleep: [],
+			awaken: []
+		};
 		var i;
 
 		var animationFrame;
 		var playing = false;
+		var dormant = false;
 		
 		var last  = 0;
 		var now   = 0;
 		var delta = 0;
 
+		var dormancyCheckInterval = null;
+
 		this.total = 0;
 
+		// Runs every frame and calls listening functions
 		var frame = function() {
 			now   = Date.now();
 			delta = now - last;
 			last  = now;
 			this.total += delta;
 
-			for (i = 0; i < listeners.length; i++)
-				listeners[i](this.total, delta);
-
+			if (!dormant) {
+				for (i = 0; i < listeners.update.length; i++)
+					listeners.update[i](this.total, delta);	
+			}
+			
 			animationFrame = window.requestAnimFrame(frame);
+		}.bind(this);
+
+		/**
+		 * Runs at 3Hz with setInterval. If requestAnimationFrame
+		 *  has fallen to 3Hz or below, it declares the updater
+		 *  asleep and calls listeners of the 'sleep' event.
+		 */
+		var dormancyCheck = function() {
+			if (!dormant) {
+				now   = Date.now();
+				delta = now - last;
+
+				if (delta > DORMANCY_THRESHOLD) {
+					dormant = true;
+
+					for (i = 0; i < listeners.sleep.length; i++)
+						listeners.sleep[i]();
+				}
+			}
+			else {
+				now   = Date.now();
+				delta = now - last;
+
+				if (delta < DORMANCY_THRESHOLD) {
+					dormant = false;
+
+					// Reset the last time so we don't build up time
+					last = now - delta;
+
+					// Call listeners
+					for (i = 0; i < listeners.awaken.length; i++)
+						listeners.awaken[i]();
+				}
+			}
 		}.bind(this);
 
 		this.play = function() {
@@ -43,7 +90,9 @@ define(function (require) {
 				 * at the same this.total time after unpausing.
 				 */
 				last = Date.now() - delta;
-				frame();	
+				clearInterval(dormancyCheckInterval);
+				dormancyCheckInterval = setInterval(dormancyCheck, DORMANCY_CHECK_INTERVAL);
+				frame();
 			}
 		};
 
@@ -61,8 +110,8 @@ define(function (require) {
 			this.total = 0;
 		};
 
-		this.addFrameListener = function(listener) {
-			listeners.push(listener);
+		this.addEventListener = function(event, listener) {
+			listeners[event].push(listener);
 		};
 	};
 
