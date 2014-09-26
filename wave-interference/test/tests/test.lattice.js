30d4a970a20ee515d31776d143e5923dceb5215a
Patrick Wolfert <patrickrwolfert@gmail.com>
2014-09-26 09:45:47 -0700

    Added more test coverage to Lattice2d

--- wave-interference/test/tests/test.lattice.js
+++ wave-interference/test/tests/test.lattice.js
@@ -92,7 +92,50 @@ describe('Lattice2D', function(){
 			[8, 5, 4],
 			[3, 8, 9]
 		];
-		lattice2.copyArea(lattice1, 3, 3, 1, 1, 0, 0);console.log(lattice2.data); console.log(expectedResult);
+		lattice2.copyArea(lattice1, 3, 3, 1, 1, 0, 0);
 		chai.expect(lattice2.data).to.deep.equal(expectedResult);
+	});
+
+	it('should find the average value around a point', function(){
+		var lattice = new Lattice2D({
+			data: [
+				[1, 0, 0, 0, 0],
+				[1, 2, 8, 1, 0],
+				[0, 5, 5, 4, 0],
+				[0, 3, 8, 9, 0],
+				[0, 0, 1, 1, 1]
+			]
+		});
+
+		chai.expect(lattice.avg(2, 2, 0)).to.equal(5);
+		chai.expect(lattice.avg(2, 2, 1)).to.equal(5);
+		chai.expect(lattice.avg(2, 2, 2)).to.equal(2);
+
+		chai.expect(lattice.avg(0, 0, 0)).to.equal(1);
+		chai.expect(lattice.avg(0, 0, 1)).to.equal(1);
+	});
+
+	it('should correctly determine whether a point is in bounds', function(){
+		var lattice1 = new Lattice2D({
+			width: 1,
+			height: 1,
+			initialValue: 0
+		});
+		var lattice2 = new Lattice2D({
+			width: 2,
+			height: 2,
+			initialValue: 0
+		});
+
+		chai.expect(lattice1.contains(-1, -1)).to.not.be.ok;
+		chai.expect(lattice1.contains( 0,  0)).to.be.ok;
+		chai.expect(lattice1.contains( 1,  1)).to.not.be.ok;
+
+		chai.expect(lattice2.contains(-1, -1)).to.not.be.ok;
+		chai.expect(lattice2.contains( 0,  0)).to.be.ok;
+		chai.expect(lattice2.contains( 0,  1)).to.be.ok;
+		chai.expect(lattice2.contains( 1,  0)).to.be.ok;
+		chai.expect(lattice2.contains( 1,  1)).to.be.ok;
+		chai.expect(lattice2.contains( 2,  2)).to.not.be.ok;
 	});
 });
