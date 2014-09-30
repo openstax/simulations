8c8afd213f90dee76263832348e5b2a43551a3d8
Patrick Wolfert <patrickrwolfert@gmail.com>
2014-09-30 14:19:03 -0700

    Resolves #12

--- wave-interference/src/js/views/sim/water.js
+++ wave-interference/src/js/views/sim/water.js
@@ -37,6 +37,33 @@ define(function(require) {
 		initHeatmapView: function() {
 			this.heatmapView = new WaterHeatmapView(this.getHeatmapViewOptions());
 		},
+
+		/**
+		 * Uses the sim view's WaveSimulation instance to determine
+		 *   appropriate options for initializing the GraphView and
+		 *   returns them as an object.
+		 */
+		getGraphViewOptions: function() {
+			return {
+				title: 'Cross-Section Side View',
+				x: {
+					start: 0,
+					end: this.waveSimulation.get('dimensions').width,
+					step: this.waveSimulation.get('dimensions').width / 10,
+					label: 'x-Position (' + this.waveSimulation.get('units').distance + ')',
+					showNumbers: true
+				},
+				y: {
+					start: -1,
+					end: 1,
+					step: 0.5,
+					label: 'Water Level',
+					showNumbers: false
+				},
+				waveSimulation: this.waveSimulation,
+				heatmapView: this.heatmapView
+			};
+		},
 	});
 
 	return WaterSimView;
