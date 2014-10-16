346569b624b49190f8888a87ee52d5ad8401f3bb
Patrick <patrickrwolfert@gmail.com>
2014-10-16 22:36:06 +0100

    Updates

---
+++ moving-man/js/utils/updater.js
@@ -0,0 +1,125 @@
+/*
+ * Based on a gist from Dan Tello
+ * https://gist.github.com/greypants/3739036
+ */
+
+define(function (require) {
+
+	'use strict';
+
+	require('pixi');
+
+	var Updater = function() {
+
+		var DORMANCY_THRESHOLD = 333;      // 3Hz in millseconds
+		var DORMANCY_CHECK_INTERVAL = 100; // ms
+
+		var listeners = {
+			update: [],
+			sleep: [],
+			awaken: []
+		};
+		var i;
+
+		var animationFrame;
+		var playing = false;
+		var dormant = false;
+		
+		var last  = 0;
+		var now   = 0;
+		var delta = 0;
+
+		var dormancyCheckInterval = null;
+
+		this.total = 0;
+
+		// Runs every frame and calls listening functions
+		var frame = function() {
+			now   = Date.now();
+			delta = now - last;
+			last  = now;
+			this.total += delta;
+
+			if (!dormant) {
+				for (i = 0; i < listeners.update.length; i++)
+					listeners.update[i](this.total, delta);	
+			}
+			else if (delta < DORMANCY_THRESHOLD)
+				awaken();
+			
+			animationFrame = window.requestAnimFrame(frame);
+		}.bind(this);
+
+		/**
+		 * Runs at 3Hz with setInterval. If requestAnimationFrame
+		 *  has fallen to 3Hz or below, it declares the updater
+		 *  asleep and calls listeners of the 'sleep' event.
+		 */
+		var dormancyCheck = function() {
+			delta = Date.now() - last;
+			if (!dormant) {
+				if (delta > DORMANCY_THRESHOLD)
+					sleep();
+			}
+			else {
+				if (delta < DORMANCY_THRESHOLD)
+					awaken();
+			}
+		}.bind(this);
+
+		var sleep = function() {
+			dormant = true;
+
+			// Call listeners
+			for (i = 0; i < listeners.sleep.length; i++)
+				listeners.sleep[i]();
+		};
+
+		var awaken = function() {
+			dormant = false;
+
+			// Reset the last time so we don't build up time
+			now   = Date.now();
+			delta = now - last;
+			last = now - delta;
+
+			// Call listeners
+			for (i = 0; i < listeners.awaken.length; i++)
+				listeners.awaken[i]();
+		};
+
+		this.play = function() {
+			if (!playing) {
+				playing = true;
+				/*
+				 * Subtract the last delta so we don't do two frames 
+				 * at the same this.total time after unpausing.
+				 */
+				last = Date.now() - delta;
+				clearInterval(dormancyCheckInterval);
+				dormancyCheckInterval = setInterval(dormancyCheck, DORMANCY_CHECK_INTERVAL);
+				frame();
+			}
+		};
+
+		this.pause = function() {
+			window.cancelAnimationFrame(animationFrame);
+			playing = false;
+		};
+
+		this.paused = function() {
+			return !playing;
+		};
+
+		this.reset = function() {
+			this.pause();
+			this.total = 0;
+		};
+
+		this.addEventListener = function(event, listener) {
+			listeners[event].push(listener);
+		};
+	};
+
+	return Updater;
+});
---
+++ moving-man/js/views/graph/light-cross-section.js
@@ -0,0 +1,130 @@
+define(function(require) {
+
+	'use strict';
+
+	var CrossSectionGraphView = require('views/graph/cross-section');
+
+	var ArrowGraphic = require('graphics/arrow');
+
+	var graphHtml    = require('text!templates/graph.html');
+	var controlsHtml = require('text!templates/light-cross-section-graph-controls.html');
+
+	/*
+	 * "Local" variables for functions to share and recycle
+	 */
+
+	/**
+	 * LightCrossSectionGraphView shows the values of a certain row of the
+	 *   lattice in real time in the form of a curve.
+	 */
+	var LightCrossSectionGraphView = CrossSectionGraphView.extend({
+
+		template: _.template(graphHtml + controlsHtml),
+
+		className: 'light-cross-section-graph-view open initial',
+
+		events: _.extend({}, CrossSectionGraphView.prototype.events, {
+			'click .curve-check'   : 'curveCheckClicked',
+			'click .vectors-check' : 'vectorsCheckClicked',
+		}),
+
+		initialize: function(options) {
+			CrossSectionGraphView.prototype.initialize.apply(this, [options]);
+
+			this.pointsPerVector = 3;
+
+			this.showCurves  = true;
+			this.showVectors = false;
+		},
+
+		/**
+		 *
+		 */
+		initVectors: function() {
+			this.vectors = [];
+
+			var numVectors = Math.floor(this.waveSimulation.lattice.width / this.pointsPerVector);
+
+			for (var i = 0; i < numVectors; i++) {
+				this.vectors.push(new ArrowGraphic({
+					context: this.context
+				}));
+			}
+		},
+
+		/**
+		 * All the graphics initializing happens at the end of the 
+		 *   render function, and we need to make sure the vectors
+		 *   get initialized.
+		 */
+		render: function() {
+			CrossSectionGraphView.prototype.render.apply(this);
+
+			this.initVectors();
+		},
+
+		/**
+		 * Renders html container
+		 */
+		renderContainer: function() {
+			this.$el.html(this.template(this.graphInfo));
+
+			this.$showButton = this.$('.graph-show-button');
+			this.$hideButton = this.$('.graph-hide-button');
+		},
+
+		/**
+		 * Draws arrows from the 0 line along the x-axis to the graph's y value
+		 *   at a given x.
+		 */
+		drawVectors: function() {
+			var y = this.height / 2;
+
+			var point;
+			for (var i = 1; i < this.vectors.length; i ++) {
+				point = this.points[i * this.pointsPerVector];
+				this.vectors[i].setTail(point.x, y);
+				this.vectors[i].setHead(point.x, point.y);
+				this.vectors[i].setLineColor(this.lineColor);
+				this.vectors[i].draw();
+			}
+		},
+
+		/**
+		 * Responds to resize events and draws everything.
+		 */
+		update: function(time, delta) {
+			if (this.resizeOnNextUpdate)
+				this.resize();
+
+			if (this.graphVisible) {
+				this.drawGraph(time, delta);
+
+				if (this.showCurves || this.showVectors)
+					this.calculatePoints(time, delta);
+
+				if (this.showCurves)
+					this.drawCurve(time, delta);	
+				if (this.showVectors)
+					this.drawVectors(time, delta);
+			}
+		},
+
+		/**
+		 *
+		 */
+		curveCheckClicked: function(event) {
+			this.showCurves = $(event.target).is(':checked');
+		},
+
+		/**
+		 *
+		 */
+		vectorsCheckClicked: function(event) {
+			this.showVectors = $(event.target).is(':checked');
+		},
+
+	});
+
+	return LightCrossSectionGraphView;
+});
