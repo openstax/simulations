fce2aba294194692fdd46cecc36d94e3f21bc78b
Patrick <patrickrwolfert@gmail.com>
2014-10-24 15:06:07 +0100

    Did I finally resolve the font paths issue?? Can I put it on github?

--- moving-man/src/js/views/app.js
+++ moving-man/src/js/views/app.js
@@ -2,13 +2,12 @@ define(function(require) {
 	
 	'use strict';
 
-	//var $        = require('jquery');
-	//var _        = require('underscore');
-
 	var AppView = require('common/app/app');
 
 	var IntroSimView  = require('views/sim/intro');
 	var ChartsSimView = require('views/sim/charts');
+
+	require('less!styles/font-awesome');
 
 	var MovingManAppView = AppView.extend({
 
