00cd5cd42bc9524b837128306e105c304c3e5a93
Patrick <patrickrwolfert@gmail.com>
2014-10-24 11:44:02 +0100

    Tests

--- moving-man/test/tests/test.data-series.js
+++ moving-man/test/tests/test.data-series.js
@@ -33,30 +33,66 @@ describe('DataSeries', function(){
 		]);
 	});
 
-	// it('should return last point', function(){
-	// 	var series = DataSeries();
-		
-	// });
+	it('should return last point', function(){
+		var series = new DataSeries();
+		series.add(5, 0);
+		series.add(10, 1);
+		series.add(15, 2);
+		series.add(20, 3);
 
-	// it('should return midpoint', function(){
-	// 	var series = DataSeries();
-		
-	// });
+		chai.expect(series.getLastPoint()).to.deep.equal({ value: 20, time: 3 });
+	});
 
-	// it('should limit size correctly', function(){
-	// 	var series = DataSeries.LimitedSize({
-	// 		maxSize: 4
-	// 	});
+	it('should return midpoint', function(){
+		var series = new DataSeries();
+		series.add(5, 0);
+		series.add(10, 1);
+		series.add(15, 2);
 
-		
-	// });
+		chai.expect(series.getMidPoint()).to.deep.equal({ value: 10, time: 1 });
 
-	// it('should limit time correctly', function(){
-	// 	var series = DataSeries.LimitedSize({
-	// 		maxSize: 4
-	// 	});
+		series.add(20, 3);
 
-		
-	// });
+		chai.expect(series.getMidPoint()).to.deep.equal({ value: 15, time: 2 });
+	});
+
+	it('should limit size correctly', function(){
+		var series = new DataSeries.LimitedSize({
+			maxSize: 3
+		});
+
+		series.add(5, 0);
+		series.add(10, 1);
+		series.add(15, 2);
+
+		chai.expect(series.getLastPoint()).to.deep.equal({ value: 15, time: 2 });
+		chai.expect(series.size()).to.equal(3);
+
+		series.add(20, 3);
+
+		chai.expect(series.getLastPoint()).to.deep.equal({ value: 20, time: 3 });
+		chai.expect(series.size()).to.equal(3);
+	});
+
+	it('should limit time correctly', function(){
+		var series = new DataSeries.LimitedSize({
+			maxTime: 4
+		});
+
+		series.add(5, 0);
+		series.add(10, 1);
+		series.add(15, 2);
+		chai.expect(series.size()).to.equal(3);
+
+		series.add(5, 0);
+		series.add(10, 1);
+		series.add(15, 2);
+		chai.expect(series.size()).to.equal(3);
+
+		series.add(25, 4);
+		series.add(64, 5);
+		series.add(11, 6);
+		chai.expect(series.size()).to.equal(5);
+	});
 
 });
