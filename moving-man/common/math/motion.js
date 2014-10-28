55674c126bc7cef1cf2e3d49e6e4416d965c33a1
Patrick <patrickrwolfert@gmail.com>
2014-10-28 16:14:55 +0000

    Updates

---
+++ moving-man/common/math/motion.js
@@ -0,0 +1,127 @@
+define(function (require) {
+
+	'use strict';
+
+	require('../node_modules/least-squares/lib/least-squares'); var leastSquares = window.lsq;
+
+	/**
+	 * Source ported from phet.common.motion.MotionMath.
+	 */
+	var MotionMath = {
+
+		/**
+		 * Cached objects
+		 */
+		_linearRegressionResults: {},
+
+		/**
+		 *
+		 */
+		estimateDerivative: function(timeSeries) {
+			var out = this.getLinearRegressionCoefficients(timeSeries);
+			if (_.isNaN(out.m) || !_.isFinite(out.m)) {
+				// PhET never finished this: "todo handle this error elsewhere"
+				return 0;
+			}
+			return out.m;
+		},
+
+		/**
+		 * Returns an object that looks like this: {
+		 *     m: line slope
+		 *     b: line intercept
+		 * }
+		 *
+		 * See https://www.npmjs.org/package/least-squares for details
+		 */
+		getLinearRegressionCoefficients: function(timeSeries) {
+			var X = [];
+			var Y = [];
+			for (var i = 0; i < timeSeries.length; i++) {
+				X[i] = timeSeries[i].time;
+				Y[i] = timeSeries[i].value;
+			}
+			return leastSquares(X, Y, false, this._linearRegressionResults);
+		},
+
+		/**
+		 *
+		 */
+		averageTime: function(timeSeries) {
+			var a = 0;
+			for (var i = 0; i < timeSeries.length; i++) {
+				a += timeSeries[i].time;
+			}
+			return a / timeSeries.length;
+		},
+
+		/**
+		 *
+		 */
+		getDerivative: function(recentPositionTimeSeries) {
+			if (recentPositionTimeSeries.length === 0) {
+				return {
+					value: 0,
+					time:  0
+				};
+			}
+			else {
+				return {
+					value: this.estimateDerivative(recentPositionTimeSeries),
+					time:  this.averageTime(recentPositionTimeSeries)
+				};
+			}
+		},
+
+		/**
+		 * PhET docs:
+		 *
+		 * "Gets the second derivative of the given time series data using the central difference formula
+		 *  See: http://mathews.ecs.fullerton.edu/n2003/NumericalDiffMod.html"
+		 */
+		getSecondDerivative: function(x) {
+			if (x.length === 0) {
+				return {
+					value: 0,
+					time:  0
+				};
+			}
+			var sum = 0;
+			var count = 0;
+			for (var i = 1; i < x.length - 1; i++) {
+				sum += this._getSecondDerivative(x[i - 1], x[i], x[i + 1]);
+				count ++;
+			}
+			if (count === 0) {
+				return {
+					value: 0,
+					time: this.averageTime(x)
+				};
+			}
+			else {
+				return {
+					value: sum / count,
+					time: this.averageTime(x)
+				};
+			}
+		},
+
+		/**
+		 *
+		 */
+		_getSecondDerivative: function(a, b, c) {
+			var num = a.value - 2*b.value + c.value;
+			var h1 = c.time - b.time;
+			var h2 = b.time - a.time;
+			var h = (h1 + h2) / 2;
+			if (h === 0)
+				throw '_getSecondDerivative: h was zero';
+			else
+				return num / (h * h);
+		}
+
+	};
+
+	return MotionMath;
+
+});
