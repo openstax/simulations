9d61a20af2abd619c4c07d026bf31acbb6f0b6fe
Patrick <patrickrwolfert@gmail.com>
2015-02-02 15:24:04 +0000

    Added a blank template sim project and modified PixiAppView so it will still load the sim if there are zero assets.

---
+++ template/src/js/views/app.js
@@ -0,0 +1,24 @@
+define(function(require) {
+    
+    'use strict';
+
+    var PixiAppView = require('common/pixi/view/app');
+
+    var TemplateSimView = require('views/sim');
+
+    var Assets = require('assets');
+
+    require('less!styles/font-awesome');
+
+    var TemplateAppView = PixiAppView.extend({
+
+        assets: Assets.getAssetList(),
+
+        simViewConstructors: [
+            TemplateSimView
+        ]
+
+    });
+
+    return TemplateAppView;
+});
