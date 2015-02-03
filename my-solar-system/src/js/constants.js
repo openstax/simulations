f82cbb1276b6215028be747a7376f2a5b322ab39
Patrick <patrickrwolfert@gmail.com>
2015-02-03 18:01:25 +0000

    Have an initial settings textbox matrix, but I'm still not sure where to position it.

--- my-solar-system/src/js/constants.js
+++ my-solar-system/src/js/constants.js
@@ -13,10 +13,18 @@ define(function (require) {
 
     //Constants.GRAVITATIONAL_ACCELERATION = 9.8; // m/s^2
     Constants.BODY_COLORS = [
-    	'#f0cf31',
-    	'#f47b4d',
-    	'#00ffff',
-    	'#71b653'
+    	// 'yellow', 
+     //    'magenta', 
+     //    'cyan',
+     //    'green'
+        // '#f0cf31',
+        // '#f47b4d',
+        // '#eeeeee',
+        // '#71b653'
+        '#D86565',
+        '#D89965',
+        '#3C8282',
+        '#50AD50'
     ];
 
 
