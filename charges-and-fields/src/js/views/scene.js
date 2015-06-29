670c09864a5cdf5659a16ba02864ef4e88eafc89
Patrick <patrickrwolfert@gmail.com>
2015-06-29 19:34:43 +0100

    Initial creation of the project

---
+++ charges-and-fields/src/js/views/scene.js
@@ -0,0 +1,46 @@
+define(function(require) {
+
+    'use strict';
+
+    var _    = require('underscore');
+    var PIXI = require('pixi');
+
+    var PixiSceneView = require('common/pixi/view/scene');
+
+    var Assets = require('assets');
+
+    // Constants
+    var Constants = require('constants');
+
+    // CSS
+    require('less!styles/scene');
+
+    /**
+     *
+     */
+    var ChargesAndFieldsSceneView = PixiSceneView.extend({
+
+        events: {
+            
+        },
+
+        initialize: function(options) {
+            PixiSceneView.prototype.initialize.apply(this, arguments);
+        },
+
+        renderContent: function() {
+            
+        },
+
+        initGraphics: function() {
+            PixiSceneView.prototype.initGraphics.apply(this, arguments);
+        },
+
+        _update: function(time, deltaTime, paused, timeScale) {
+            
+        },
+
+    });
+
+    return ChargesAndFieldsSceneView;
+});
