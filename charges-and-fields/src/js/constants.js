de4e8c2663ff5ab8dd4b6635b0d71d7a62a147b1
Patrick <patrickrwolfert@gmail.com>
2015-06-30 11:03:44 +0100

    I've tried to figure out where they're converting units, but not all of the code is kept together, and it's difficult to track.  Their grid is based on meters, but I think it's actually completely independent of the model and independent of any views.  Everything else uses flash units, and it seems to be arbitrary.  Right now I have the model set to use meters, and the grid is based on that, but I may change it to some arbitrary unit because the grid doesn't even look nice anyway.

--- charges-and-fields/src/js/constants.js
+++ charges-and-fields/src/js/constants.js
@@ -11,9 +11,28 @@ define(function (require) {
      **                                                                     **
      *************************************************************************/
 
-    //Constants.GRAVITATIONAL_ACCELERATION = 9.8; // m/s^2
+    Constants.SIM_HEIGHT_IN_METERS = 4.5; // Meters
 
 
+
+    /*************************************************************************
+     **                                                                     **
+     **                                SCENE                                **
+     **                                                                     **
+     *************************************************************************/
+
+    var SceneView = {};
+
+    SceneView.PANEL_BG = '#ededed';
+    SceneView.PANEL_MARGIN = 20;
+    SceneView.SHORT_SCREEN_PANEL_MARGIN = 13;
+
+    SceneView.GRID_COLOR = '#000';
+    SceneView.GRID_ALPHA = 0.15;
+    SceneView.GRID_MAJOR_SIZE_IN_METERS = 0.5;
+    SceneView.GRID_MINOR_SIZE_IN_METERS = 0.1;
+
+    Constants.SceneView = SceneView;
 
     return Constants;
 });
--- charges-and-fields/src/js/views/scene.js
+++ charges-and-fields/src/js/views/scene.js
@@ -5,12 +5,18 @@ define(function(require) {
     var _    = require('underscore');
     var PIXI = require('pixi');
 
-    var PixiSceneView = require('common/pixi/view/scene');
+    var PixiSceneView      = require('common/pixi/view/scene');
+    var GridView           = require('common/pixi/view/grid');
+    var AppView            = require('common/app/app');
+    var ModelViewTransform = require('common/math/model-view-transform');
+    var Vector2            = require('common/math/vector2');
+    var Rectangle          = require('common/math/rectangle');
 
     var Assets = require('assets');
 
     // Constants
     var Constants = require('constants');
+    var SceneView = Constants.SceneView;
 
     // CSS
     require('less!styles/scene');
@@ -34,6 +40,45 @@ define(function(require) {
 
         initGraphics: function() {
             PixiSceneView.prototype.initGraphics.apply(this, arguments);
+
+            this.initMVT();
+            this.initGrid();
+        },
+
+        initMVT: function() {
+            var heightInMeters = Constants.SIM_HEIGHT_IN_METERS;
+            var scale = (this.height - 2) / heightInMeters;
+            var widthInMeters = Math.ceil(this.width / scale);
+            
+            this.viewOriginX = -(widthInMeters / 2) * scale;
+            this.viewOriginY = 0;
+
+            this.mvt = ModelViewTransform.createSinglePointScaleInvertedYMapping(
+                new Vector2(0, 0),
+                new Vector2(this.viewOriginX, this.viewOriginY),
+                scale
+            );
+        },
+
+        initGrid: function() {
+            this.gridView = new GridView({
+                origin: new Vector2(this.viewOriginX, this.viewOriginY),
+                bounds: new Rectangle(0, 0, this.width, this.height),
+
+                gridSize:         this.mvt.modelToViewDeltaX(SceneView.GRID_MAJOR_SIZE_IN_METERS),
+                smallGridSize:    this.mvt.modelToViewDeltaX(SceneView.GRID_MINOR_SIZE_IN_METERS),
+                smallGridEnabled: true,
+
+                lineWidth: 2,
+                lineColor: SceneView.GRID_COLOR,
+                lineAlpha: SceneView.GRID_ALPHA,
+
+                smallLineColor: SceneView.GRID_COLOR,
+                smallLineWidth: 1,
+                smallLineAlpha: SceneView.GRID_ALPHA
+            });
+            //this.gridView.hide();
+            this.stage.addChild(this.gridView.displayObject);
         },
 
         _update: function(time, deltaTime, paused, timeScale) {
