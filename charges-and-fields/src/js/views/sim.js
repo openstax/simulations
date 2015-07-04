c778aefca6e37fd11c42409f7f9189ab03ec1444
Patrick <patrickrwolfert@gmail.com>
2015-07-04 12:12:23 +0100

    Added measuring tape, and it toggles correctly with the Numbers and Tape Measure checkboxes.

--- charges-and-fields/src/js/views/sim.js
+++ charges-and-fields/src/js/views/sim.js
@@ -5,7 +5,8 @@ define(function (require) {
     var $ = require('jquery');
     var _ = require('underscore');
 
-    var SimView = require('common/app/sim');
+    var SimView           = require('common/app/sim');
+    var MeasuringTapeView = require('common/tools/measuring-tape');
 
     var ChargesAndFieldsSimulation = require('models/simulation');
     var ChargesAndFieldsSceneView  = require('views/scene');
@@ -72,6 +73,7 @@ define(function (require) {
             SimView.prototype.initialize.apply(this, [options]);
 
             this.initSceneView();
+            this.initMeasuringTapeView();
         },
 
         /**
@@ -91,6 +93,24 @@ define(function (require) {
         },
 
         /**
+         * Initializes the MeasuringTapeView.
+         */
+        initMeasuringTapeView: function() {
+            this.measuringTapeView = new MeasuringTapeView({
+                dragFrame: this.el,
+                viewToModelDeltaX: _.bind(function(dx){
+                    return this.sceneView.mvt.viewToModelDeltaX(dx);
+                }, this),
+                viewToModelDeltaY: _.bind(function(dy){
+                    return this.sceneView.mvt.viewToModelDeltaY(dy);
+                }, this)
+            });
+            this.listenTo(this.sceneView, 'change:mvt', function() {
+                this.measuringTapeView.updateOnNextFrame = true;
+            });
+        },
+
+        /**
          * Renders everything
          */
         render: function() {
@@ -98,6 +118,7 @@ define(function (require) {
 
             this.renderScaffolding();
             this.renderSceneView();
+            this.renderMeasuringTape();
 
             return this;
         },
@@ -123,11 +144,24 @@ define(function (require) {
         },
 
         /**
+         * Renders the measuring tape view
+         */
+        renderMeasuringTape: function() {
+            this.measuringTapeView.render();
+            this.$el.append(this.measuringTapeView.el);
+        },
+
+        /**
          * Called after every component on the page has rendered to make sure
          *   things like widths and heights and offsets are correct.
          */
         postRender: function() {
             this.sceneView.postRender();
+
+            this.measuringTapeView.postRender();
+            this.measuringTapeView.setStart(this.sceneView.mvt.modelToViewX(4), this.sceneView.mvt.modelToViewY(5.1));
+            this.measuringTapeView.setEnd(  this.sceneView.mvt.modelToViewX(7), this.sceneView.mvt.modelToViewY(5.1));
+            this.measuringTapeView.hide();
         },
 
         /**
@@ -151,6 +185,9 @@ define(function (require) {
 
             // Update the scene
             this.sceneView.update(timeSeconds, dtSeconds, this.simulation.get('paused'));
+
+            // Update the measuring tape view
+            this.measuringTapeView.update(timeSeconds, dtSeconds, this.simulation.get('paused'));
         },
 
         /**
@@ -206,11 +243,14 @@ define(function (require) {
             if ($(event.target).is(':checked')) {
                 this.$('.numbers-additional-options').removeClass('disabled');
                 this.$('#tape-measure-check').removeAttr('disabled');
+                if (this.$('#tape-measure-check').is(':checked'))
+                    this.measuringTapeView.show();
                 this.sceneView.showNumbers();
             }
             else {
                 this.$('.numbers-additional-options').addClass('disabled');
                 this.$('#tape-measure-check').prop('disabled', true);
+                this.measuringTapeView.hide();
                 this.sceneView.hideNumbers();
             }
         },
@@ -219,10 +259,10 @@ define(function (require) {
          * Shows/hides tape measure
          */
         toggleTapeMeasure: function() {
-            // if ($(event.target).is(':checked'))
-                
-            // else
-                
+            if ($(event.target).is(':checked'))
+                this.measuringTapeView.show();
+            else
+                this.measuringTapeView.hide();
         },
 
         /**
