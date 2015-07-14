65ac1bfbe07d459774f3d889416ee34fb0a16b9a
Patrick <patrickrwolfert@gmail.com>
2015-07-14 16:19:33 +0100

    Plate area drag handle looks good. Now for the separation drag handle.

--- capacitor-lab/src/js/views/capacitor.js
+++ capacitor-lab/src/js/views/capacitor.js
@@ -35,7 +35,14 @@ define(function(require) {
             'touchend        .plateSeparationHandle': 'dragPlateSeparationEnd',
             'mouseup         .plateSeparationHandle': 'dragPlateSeparationEnd',
             'touchendoutside .plateSeparationHandle': 'dragPlateSeparationEnd',
-            'mouseupoutside  .plateSeparationHandle': 'dragPlateSeparationEnd'
+            'mouseupoutside  .plateSeparationHandle': 'dragPlateSeparationEnd',
+
+            'mouseover .plateAreaHandle'  : 'plateAreaHover',
+            'mouseout  .plateAreaHandle'  : 'plateAreaUnhover',
+            //'mousedown .plateAreaHandle'  : 'plateAreaDown',
+            'mouseover .plateSeparationHandle' : 'plateSeparationHover',
+            'mouseout  .plateSeparationHandle' : 'plateSeparationUnhover',
+            //'mousedown .plateSeparationHandle' : 'plateSeparationBtnDown',
         },
 
         initialize: function(options) {
@@ -47,7 +54,10 @@ define(function(require) {
                 labelFontFamily: 'Helvetica Neue',
                 labelFontSize: '16px',
                 labelColor: '#000',
-                labelAlpha: 1
+                labelAlpha: 1,
+
+                handleColor: '#5c35a3',
+                handleHoverColor: '#955cff'
             }, options);
 
             this.mvt = options.mvt;
@@ -61,10 +71,15 @@ define(function(require) {
             this.labelColor = options.labelColor;
             this.labelAlpha = options.labelAlpha;
 
+            this.handleColor = Colors.parseHex(options.handleColor);
+            this.handleHoverColor = Colors.parseHex(options.handleHoverColor);
+
             // Cached objects
             this._dragOffset   = new PIXI.Point();
             this._dragLocation = new PIXI.Point();
             this._position = new Vector2();
+            this._ll = new Vector2();
+            this._ur = new Vector2();
 
             // Initialize graphics
             this.initGraphics();
@@ -85,7 +100,8 @@ define(function(require) {
             this.displayObject.addChild(this.topLayer);
 
             this.initPlates();
-            this.initResizeHandles();
+            this.initPlateAreaHandle();
+            this.initPlateSeparationHandle();
             this.initDielectric();
             
             this.updateMVT(this.mvt);
@@ -99,14 +115,48 @@ define(function(require) {
             this.bottomLayer.addChild(this.bottomPlate);
         },
 
-        initResizeHandles: function() {
-            var plateAreaHandle = new PIXI.Graphics();
+        initPlateAreaHandle: function() {
+            // The rotation should make it look like it's going out diagonally
+            //   from the lower left corner of the top plate and planar with it.
+            var lowerLeft  = this.getPlateDragCorner();
+            var upperRight = this.getOppositePlateDragCorner();
+            var rotation = Math.atan2(upperRight.y - lowerLeft.y, upperRight.x - upperRight.y);
 
+            this.plateAreaHandleGraphic = new PIXI.Graphics();
+            this.plateAreaHandleHoverGraphic = new PIXI.Graphics();
+
+            this.drawDragHandle(this.plateAreaHandleGraphic,      this.handleColor);
+            this.drawDragHandle(this.plateAreaHandleHoverGraphic, this.handleHoverColor);
+
+            this.plateAreaHandleGraphic.hitArea = new PIXI.Circle(0, 0, 17);
+            this.plateAreaHandleHoverGraphic.hitArea = new PIXI.Circle(0, 0, 17);
+
+            this.plateAreaHandleGraphic.x = this.plateAreaHandleHoverGraphic.x = -32;
+
+            var dots = new PIXI.Graphics();
+            dots.beginFill(0x000000, 1);
+            for (var x = -14; x < 0; x += 4)
+                dots.drawCircle(x, 0, 1);
+            dots.endFill();
+
+            var graphicsWrapper = new PIXI.DisplayObjectContainer();
+            graphicsWrapper.rotation = rotation;
+            graphicsWrapper.addChild(this.plateAreaHandleGraphic);
+            graphicsWrapper.addChild(this.plateAreaHandleHoverGraphic);
+            graphicsWrapper.addChild(dots);
+
+            var plateAreaHandle = new PIXI.DisplayObjectContainer();
             plateAreaHandle.buttonMode = true;
-            plateAreaHandle.defaultCursor = 'move';
+            plateAreaHandle.x = lowerLeft.x;
+            plateAreaHandle.y = lowerLeft.y;
+            //plateAreaHandle.hitArea = new PIXI.Circle(0, 0, 16);
+            plateAreaHandle.addChild(graphicsWrapper);
 
             this.plateAreaHandle = plateAreaHandle;
+            this.displayObject.addChild(plateAreaHandle);
+        },
 
+        initPlateSeparationHandle: function() {
             var plateSeparationHandle = new PIXI.Graphics();
 
             plateSeparationHandle.buttonMode = true;
@@ -126,44 +176,74 @@ define(function(require) {
             // label.y = 0;
         },
 
+        drawDragHandle: function(graphics, color) {
+            var length = 16;
+            var headLength = 10;
+            var headWidth = 14;
+            var tailWidth = 6;
+
+            graphics.beginFill(color, 1);
+            graphics.drawRect(0, -tailWidth / 2, length - headLength, tailWidth);
+            graphics.drawRect(-length + headLength, -tailWidth / 2, length - headLength, tailWidth);
+            graphics.endFill();
+
+            graphics.beginFill(color, 1);
+            graphics.moveTo(length, 0);
+            graphics.lineTo(length - headLength,  headWidth / 2);
+            graphics.lineTo(length - headLength, -headWidth / 2);
+            graphics.endFill();
+
+            graphics.beginFill(color, 1);
+            graphics.moveTo(-length, 0);
+            graphics.lineTo(-length + headLength,  headWidth / 2);
+            graphics.lineTo(-length + headLength, -headWidth / 2);
+            graphics.endFill();
+        },
+
+        getPlateDragCorner: function() {
+            var x = this.model.get('position').x;
+            var y = this.model.getTopPlateCenter().y;
+            var z = this.model.get('position').z;
+            var width = this.model.get('plateWidth');
+            var depth = this.model.get('plateDepth');
+            return this._ur.set(this.mvt.modelToView(
+                x - (width / 2), 
+                y, 
+                z - (depth / 2))
+            );
+        },
+
+        getOppositePlateDragCorner: function() {
+            var x = this.model.get('position').x;
+            var y = this.model.getTopPlateCenter().y;
+            var z = this.model.get('position').z;
+            var width = this.model.get('plateWidth');
+            var depth = this.model.get('plateDepth');
+            return this._ll.set(this.mvt.modelToView(
+                x + (width / 2), 
+                y, 
+                z + (depth / 2))
+            );
+        },
+
         initDielectric: function() {
             
         },
 
         drawPlates: function() {
             this.bottomPlate.clear();
-            //this.bottomPlate.lineStyle(1, 0x000000, 1);
-            // this.bottomPlate.beginFill(0xFF5555, 1);
-            // this.bottomPlate.drawPiecewiseCurve(this.shapeCreator.createBottomPlateShape());
-            // this.bottomPlate.endFill();
             this.shapeCreator.drawBottomPlateShape(this.bottomPlate, '#f2f2f2', 1);
             this.shapeCreator.outlineBottomPlateShape(this.bottomPlate, 1, this.outlineColor, 1);
 
             this.topPlate.clear();
-            // this.topPlate.lineStyle(1, 0x000000, 1);
-            // this.topPlate.beginFill(0x5555FF, 1);
-            // this.topPlate.drawPiecewiseCurve(this.shapeCreator.createTopPlateShape());
-            // this.topPlate.endFill();
             this.shapeCreator.drawTopPlateShape(this.topPlate, '#f2f2f2', 1);
             this.shapeCreator.outlineTopPlateShape(this.topPlate, 1, this.outlineColor, 1);
-        },
-
-        moveToTop: function() {
-            var parent = this.displayObject.parent;
-            parent.setChildIndex(this.displayObject, parent.children.length - 1);
-        },
-
-        updatePosition: function(model, position) {
-            // var viewPos = this.mvt.modelToView(position);
-            // this.displayObject.x = viewPos.x;
-            // this.displayObject.y = viewPos.y;
         },
 
         updateMVT: function(mvt) {
             this.mvt = mvt;
 
             this.drawPlates();
-            this.updatePosition(this.model, this.model.get('position'));
         },
 
         dragPlateAreaStart: function(data) {
@@ -196,6 +276,16 @@ define(function(require) {
 
         dragPlateSeparationEnd: function(data) {
             this.draggingPlateSeparation = false;
+        },
+
+        plateAreaHover: function() {
+            this.plateAreaHandleGraphic.visible = false;
+            this.plateAreaHandleHoverGraphic.visible = true;
+        },
+
+        plateAreaUnhover: function() {
+            this.plateAreaHandleGraphic.visible = true;
+            this.plateAreaHandleHoverGraphic.visible = false;
         },
 
         /**
