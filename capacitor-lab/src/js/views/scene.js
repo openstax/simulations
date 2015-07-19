e3980dfc955ad02b023e9b0cd4eb333b18926186
Patrick <patrickrwolfert@gmail.com>
2015-07-19 21:14:44 +0100

    Made the mvt dynamic

--- capacitor-lab/src/js/views/scene.js
+++ capacitor-lab/src/js/views/scene.js
@@ -7,8 +7,10 @@ define(function(require) {
     require('common/pixi/extensions');
 
     var PixiSceneView        = require('common/pixi/view/scene');
+    var AppView              = require('common/app/app');
     var ModelViewTransform3D = require('common/math/model-view-transform-3d');
     var Vector2              = require('common/math/vector2');
+    var Rectangle            = require('common/math/rectangle');
 
     var Assets = require('assets');
 
@@ -43,26 +45,22 @@ define(function(require) {
 
         initMVT: function() {
             // Map the simulation bounds...
-            // var bounds = this.simulation.bounds;
+            var bounds = new Rectangle(0, 0, 0.06, 0.055);
 
             // // ...to the usable screen space that we have
-            // var controlsWidth = 180;
-            // var margin = 20;
-            // var leftMargin = AppView.windowIsShort() ? margin + controlsWidth + margin : margin;
-            // var rightMargin = margin + controlsWidth + margin;
-            // var usableScreenSpace = new Rectangle(leftMargin, 0, this.width - leftMargin - rightMargin, this.height);
+            var controlsWidth = 220;
+            var margin = 20;
+            var leftMargin = AppView.windowIsShort() ? margin + controlsWidth + margin : margin;
+            var rightMargin = margin + controlsWidth + margin;
+            var usableScreenSpace = new Rectangle(leftMargin, 0, this.width - leftMargin - rightMargin, this.height);
 
-            // var boundsRatio = bounds.w / bounds.h;
-            // var screenRatio = usableScreenSpace.w / usableScreenSpace.h;
+            var boundsRatio = bounds.w / bounds.h;
+            var screenRatio = usableScreenSpace.w / usableScreenSpace.h;
             
-            // var scale = (screenRatio > boundsRatio) ? usableScreenSpace.h / bounds.h : usableScreenSpace.w / bounds.w;
+            var scale = (screenRatio > boundsRatio) ? usableScreenSpace.h / bounds.h : usableScreenSpace.w / bounds.w;
             
-            // this.viewOriginX = Math.round(usableScreenSpace.x + usableScreenSpace.w / 2);
-            // this.viewOriginY = Math.round(usableScreenSpace.y + usableScreenSpace.h);
-
-            this.viewOriginX = 15;
-            this.viewOriginY = 15;
-            var scale = 12000;
+            this.viewOriginX = Math.round(usableScreenSpace.x);
+            this.viewOriginY = Math.round(usableScreenSpace.y);
 
             this.mvt = ModelViewTransform3D.createSinglePointScaleMapping(
                 new Vector2(0, 0),
