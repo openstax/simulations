5cae78d27d0a0b7ded6e8b7363f811c49c274ec6
Patrick <patrickrwolfert@gmail.com>
2015-08-14 15:23:49 +0100

    Working on the electron and movement strategy.  I've added a dependency for a median filter, which I guess is being used for noise reduction. I realized, though, that it creates a new array each time it's called, but looking at the source, it's no less efficient than the original because a temp array has to be created.

--- radio-waves/src/js/config.js
+++ radio-waves/src/js/config.js
@@ -10,11 +10,13 @@
             text:       '../../bower_components/requirejs-text/text',
             pixi:       '../../bower_components/pixi/bin/pixi',
             nouislider: '../../bower_components/nouislider/distribute/jquery.nouislider.all.min',
-            'vector2-node':   '../../node_modules/vector2-node-shimmed/index',
-            'object-pool':    '../../node_modules/object-pool-shimmed/index',
-            'circular-list':  '../../node_modules/object-pool-shimmed/node_modules/circular-list/index',
+            
+            'vector2-node':          '../../node_modules/vector2-node-shimmed/index',
+            'object-pool':           '../../node_modules/object-pool-shimmed/index',
+            'circular-list':         '../../node_modules/object-pool-shimmed/node_modules/circular-list/index',
             'bootstrap-select':      '../../node_modules/bootstrap-select/js/bootstrap-select',
             'bootstrap-select-less': '../../node_modules/bootstrap-select/less/bootstrap-select',
+            'filters':               '../../node_modules/filters-shimmed/index',
 
             views:      '../js/views',
             models:     '../js/models',
--- radio-waves/src/js/models/electron.js
+++ radio-waves/src/js/models/electron.js
@@ -39,9 +39,9 @@ define(function (require) {
             MotionObject.prototype.initialize.apply(this, [attributes, options]);
 
             this.positionHistory = [];
-            this.startPosition   = new Vector2(options.startPosition);
-            this.currentPosition = new Vector2(this.startPosition);
+            this.startPosition    = new Vector2(options.startPosition);
             this.previousPosition = new Vector2();
+            this.setPosition(this.startPosition);
 
             this.accelerationHistory = [];
             // The history of the maximum acceleration the electron courld have had
@@ -53,8 +53,8 @@ define(function (require) {
             this.movementStrategyHistory = [];
             this.movementStrategy = new ManualMovement();
 
-            this.staticFieldStregnth  = new Vector2;
-            this.dynamicFieldStrength = new Vector2;
+            this._staticFieldStrength  = new Vector2;
+            this._dynamicFieldStrength = new Vector2;
 
             this.recordingHistory = true;
 
@@ -71,14 +71,14 @@ define(function (require) {
         update: function(time, deltaTime) {
             var dt = deltaTime;
 
-            this.previousPosition.set(this.currentPosition);
+            this.previousPosition.set(this.get('position'));
 
             // Update the movement strategy and our resulting velocity
             this.movementStrategy.update(dt);
             this.setVelocity(this.movementStrategy.getVelocity());
 
             if (this.recordingHistory) {
-                this.recordPosition(this.currentPosition);
+                this.recordPosition(this.get('position'));
             }
 
             // If the electron is using sinusoidal movement, we need to see if it's
@@ -102,13 +102,36 @@ define(function (require) {
                 // If we have an amplitude change pending, determine if this is the
                 //   right time to make it
                 if (this.changeAmplitude) {
-                    if ((this.previousPosition.y - this.startPosition.y) * (this.currentPosition.y - this.startPosition.y) <= 0) {
+                    if ((this.previousPosition.y - this.startPosition.y) * (this.get('position').y - this.startPosition.y) <= 0) {
                         ms.setAmplitude(this.newAmplitude);
                         this.changeAmplitude = false;
                     }
                 }
             }
             
+        },
+
+        /**
+         *
+         */
+        recordPosition: function(position) {
+            var i;
+
+            for (i = RETARDED_FIELD_LENGTH - 1; i > STEP_SIZE - 1; i--) {
+                this.positionHistory[i].set(this.positionHistory[i - STEP_SIZE]);
+                this.accelerationHistory[i].set(this.accelerationHistory[i - STEP_SIZE]);
+                this.maxAccelerationHistory[i].set(this.maxAccelerationHistory[i - STEP_SIZE]);
+                this.movementStrategyHistory[i] = this.movementStrategyHistory[i - STEP_SIZE];
+            }
+
+            var a = accelerationHistory[0];
+            var df = (a.y - movementStrategy.getAcceleration() * B) / STEP_SIZE;
+            for (i = 0; i < STEP_SIZE; i++) {
+                this.positionHistory[i].set(position);
+                this.accelerationHistory[i].y = this.movementStrategy.getAcceleration() * B + i * df;
+                this.maxAccelerationHistory[i].y = this.movementStrategy.getMaxAcceleration() * B;
+                this.movementStrategyHistory[i] = this.movementStrategy;
+            }
         },
 
         /**
@@ -121,20 +144,142 @@ define(function (require) {
         },
 
         /**
-         * Gets center of mass
+         * Returns the center of mass
          */
-        getCenterOfMass: function() {},
+        getCenterOfMass: function() {
+            return this.get('position');
+        },
 
         /**
-         * Gets moment of inertia
+         * Returns the moment of inertia
          */
-        getMomentOfInertia: function() {},
+        getMomentOfInertia: function() {
+            return 0;
+        },
+
+        /**
+         * Returns the rest mass
+         */
+        getRestMass: function() {
+            return REST_MASS;
+        },
 
         /**
          * Calculates and returns the mass of the electron
          */
         getMass: function() {
+            //mr = m0 /sqrt(1 - v2/c2)
+            var vMag = this.get('velocity').length();
+            var denom = Math.sqrt(1 - (vMag * vMag) / (Math.pow(Constants.SPEED_OF_LIGHT, 2)));
+            var mr = REST_MASS / denom;
+            return mr;
+        },
 
+        /**
+         * Returns the y acceleration from historical acceleration values from
+         *   a history-array index. The given history index must be an integer.
+         */
+        getAccelerationAt: function(x) {
+            return this.accelerationHistory[Math.min(x, this.accelerationHistory.length - 1)].y;
+        },
+
+        /**
+         * Returns the y position from historical position values from either
+         *   a history-array index or a 2D point.
+         */
+        getPositionAt: function(x) {
+            if (_.isObject(x)) {
+                return this.getPositionAt(Math.floor(x.distance(this.get('position'))));
+            }
+            else
+                return this.positionHistory[Math.min(x, this.positionHistory.length - 1)].y;
+        },
+
+        /**
+         * Returns the movement strategy from history from a point in 2D space.
+         */
+        getMovementTypeAt: function(location) {
+            var x = Math.floor(location.distance(this.get('position')));
+            return this.movementStrategyHistory[x];
+        },
+
+        /**
+         * Returns a the static electric field at a specified point. Note that to minimize
+         *   memory allocation, the Vector2 returned is re-used by every call to this method.
+         */
+        getStaticFieldAt: function(location) {
+            var staticFieldStrength = this._staticFieldStrength;
+
+            staticFieldStrength.x = location.x - this.get('position').x;
+            staticFieldStrength.y = location.y - this.get('position').y;
+            staticFieldStrength.normalize();
+
+            var distanceFromSource = location.distance(this.get('position'));
+            staticFieldStrength.scale(B * STATIC_FIELD_SCALE / (distanceFromSource * distanceFromSource));
+
+            return staticFieldStrength;
+        },
+
+        /**
+         * Returns a the dynamic electric field at a specified point. Note that to minimize
+         *   memory allocation, the Vector2 returned is re-used by every call to this method.
+         */
+        getDynamicFieldAt: function(location) {
+            var fieldStrength = this._dynamicFieldStrength;
+
+            // Hollywood here!  This computes the field based on the origin of the
+            //   electron's motion, not its current position. But it looks better.
+            var distanceFromSource = location.distance(this.startPosition);
+            if (distanceFromSource === 0)
+                throw 'Asked for r=0 field.';
+
+            var generatingPos = this.positionHistory[parseInt(distanceFromSource)];
+
+            // Determine the direction of the field.
+            fieldStrength.x = -(location.y - generatingPos.y);
+            if (location.x - generatingPos.x < 0)
+                fieldStrength.x = -fieldStrength.x;
+            fieldStrength.y = Math.abs(location.x - generatingPos.x);
+            fieldStrength.normalize();
+
+            // Set the magnitude of the field to the acceleration of the electron, reduced
+            //   by the by the distance from the source.
+            var acceleration = this.getAccelerationAt(parseInt(distanceFromSource));
+
+            var distanceScaleFactor;
+            if (distanceFromSource === 0)
+                distanceScaleFactor = 1;
+            else
+                distanceScaleFactor = Math.pow(distanceFromSource, 0.5);
+            fieldStrength.scale(acceleration / distanceScaleFactor);
+
+            // The following factor is used to give the fall-off associated with being off-axis.
+            if ( distanceFromSource == 0.0 ) {
+                distanceFromSource = 1;
+            }
+            var dubsonFactor = Math.abs(location.x - this.startPosition.x) / distanceFromSource;
+            fieldStrength.scale(dubsonFactor);
+
+            return fieldStrength;
+        },
+
+        setFrequency: function(freq) {
+            if (this.movementStrategy instanceof SinusoidalMovement) {
+                this.changeFreq = true;
+                this.newFreq = freq;
+            }
+        },
+
+        setAmplitude: function(amp) {
+            if (this.movementStrategy instanceof SinusoidalMovement) {
+                this.changeAmplitude = true;
+                this.newAmplitude = amp;
+            }
+        },
+
+        moveToNewPosition: function(newPosition) {
+            if (this.movementStrategy instanceof ManualMovement)
+                this.movementStrategy.setPosition(newPosition);
         }
 
     });
