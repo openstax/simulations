b785996da62bb7f18c19ebfe82068af524902587
Patrick Wolfert <patrickrwolfert@gmail.com>
2015-08-22 20:06:33 +0100

    Media data and dispersion functions

--- bending-light/src/js/constants.js
+++ bending-light/src/js/constants.js
@@ -2,7 +2,6 @@ define(function (require) {
 
     'use strict';
 
-
     var Constants = {}; 
 
     /*************************************************************************
@@ -11,7 +10,32 @@ define(function (require) {
      **                                                                     **
      *************************************************************************/
 
-    //Constants.GRAVITATIONAL_ACCELERATION = 9.8; // m/s^2
+    
+    Constants.SPEED_OF_LIGHT = 2.99792458E8;
+    Constants.WAVELENGTH_RED = 650E-9;
+
+    // To come up with a good time scale dt, use lambda = v/f.  
+    // For lambda = RED_WAVELENGTH and C=SPEED_OF_LIGHT, we have f=4.612E14
+    Constants.RED_LIGHT_FREQUENCY = SPEED_OF_LIGHT / WAVELENGTH_RED;
+
+    // Speed up by a factor of 2.5 because default wave view was moving too slow
+    Constants.TIME_SPEEDUP_SCALE = 2.5; 
+
+    // Thirty frames per cycle times the speedup scale
+    Constants.MAX_DT = 1.0 / RED_LIGHT_FREQUENCY / 30 * TIME_SPEEDUP_SCALE;
+    Constants.MIN_DT = MAX_DT / 10;
+    Constants.DEFAULT_DT = MAX_DT / 4;
+
+    // A good size for the units being used in the sim; used to determine the 
+    //   dimensions of various model objects
+    Constants.CHARACTERISTIC_LENGTH = WAVELENGTH_RED;
+
+
+
+    // Materials
+    Constants.DEFAULT_LASER_DISTANCE_FROM_PIVOT = 8.125E-6;
+    Constants.DIAMOND_INDEX_OF_REFRACTION_FOR_RED_LIGHT = 2.419;
+
 
 
 
---
+++ bending-light/src/js/models/dispersion-function.js
@@ -0,0 +1,84 @@
+define(function (require) {
+
+    'use strict';
+
+    var _ = require('underscore');
+
+    var Constants = require('constants');
+
+    /**
+     * Original PhET documentation by Sam Reid:
+     *   Models dispersion functions for each material.  Uses the actual dispersion
+     *   equation for air (A) and the actual dispersion equation for glass (G) then
+     *   interpolates between the functions 
+     *             n(lambda) = beta * A(lambda) + (1-beta) * G(lambda) 
+     *   where 0<=beta<=infinity is a characteristic of the material.  The material
+     *   is characterized by a reference wavelength, so that when light is the
+     *   specified wavelength, the index of refraction takes the reference value.
+     */
+    var DispersionFunction = function(referenceIndexOfRefraction, wavelength) {
+        this.referenceIndexOfRefraction = referenceIndexOfRefraction;
+        // If only the reference index of refraction is given, we assume it's
+        //   the index of refraction for the red wavelength.
+        this.referenceWavelength = (wavelength !== undefined) ? wavelength : Constants.WAVELENGTH_RED;
+    };
+
+    /**
+     * Instance functions/properties
+     */
+    _.extend(DispersionFunction.prototype, {
+
+        getIndexOfRefractionForRed: function() {
+            return this.getIndexOfRefraction(Constants.WAVELENGTH_RED);
+        },
+
+        /**
+         * See class-level documentation for an explanation of this algorithm
+         */
+        getIndexOfRefraction: function(wavelength) {
+            // Get the reference values
+            var nAirReference = DispersionFunction.getAirIndex(this.referenceWavelength);
+            var nGlassReference = DispersionFunction.getSellmeierValue(this.referenceWavelength);
+
+            // Determine the mapping and make sure it is in a good range
+            var delta = nGlassReference - nAirReference;
+            var x = (this.referenceIndexOfRefraction - nAirReference) / delta; // 0 to 1 (air to glass)
+            x = Math.max(0, Math.min(x, Number.POSITIVE_INFINITY));
+
+            // Take a linear combination of glass and air equations
+            var index = x * DispersionFunction.getSellmeierValue(wavelength) + (1 - x) * DispersionFunction.getAirIndex(wavelength);
+            return index;
+        }
+
+    });
+
+    /**
+     * Static functions/properties
+     */
+    _.extend(DispersionFunction, {
+
+        /**
+         * See http://en.wikipedia.org/wiki/Sellmeier_equation
+         */
+        getSellmeierValue: function(wavelength) {
+            var L2 = wavelength * wavelength;
+            var B1 = 1.03961212;
+            var B2 = 0.231792344;
+            var B3 = 1.01046945;
+            var C1 = 6.00069867E-3 * 1E-12; // Convert to metric
+            var C2 = 2.00179144E-2 * 1E-12;
+            var C3 = 1.03560653E2 * 1E-12;
+            return Math.sqrt(1 + B1 * L2 / (L2 - C1) + B2 * L2 / (L2 - C2) + B3 * L2 / (L2 - C3));
+        },
+
+        /**
+         * See http://refractiveindex.info/?group=GASES&material=Air
+         */
+        getAirIndex: function(wavelength) {
+            return 1 + 5792105E-8 / (238.0185 - Math.pow(wavelength * 1E6, -2)) + 167917E-8 / (57.362 - Math.pow(wavelength * 1E6, -2));
+        }
+
+    });
+
+    return DispersionFunction;
+});
