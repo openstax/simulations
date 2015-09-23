9dc58dcf46aa9a690ea6a3c304005f49d186b518
Patrick Wolfert <patrickrwolfert@gmail.com>
2015-09-23 15:14:19 +0100

    It looks like a lot less info will be needed in AverageCurrent

--- battery-resistor-circuit/src/js/models/law/average-current.js
+++ battery-resistor-circuit/src/js/models/law/average-current.js
@@ -11,10 +11,8 @@ define(function (require) {
     /**
      * 
      */
-    var AverageCurrent = function(numSamples, region) {
-        this.particle = [];
+    var AverageCurrent = function(numSamples) {
         this.series = new NumberSeries(numSamples);
-        this.region = region;
         this.resistance = 0;
         this.voltage = 0;
         this.display = 0;
@@ -26,24 +24,9 @@ define(function (require) {
     _.extend(AverageCurrent.prototype, Law.prototype, {
         
         update: function(deltaTime, system) {
-            var sum = 0;
-            var n = 0;
-            for (var i = 0; i < this.particles.length; i++) {
-                var particle = this.particles[i];
-                if (this.region.contains(particle)) {
-                    sum += particle.velocity * particle.charge;
-                    n++;
-                }
-            }
-
-            if (n !== 0)
-                sum /= n;
-            
             var hollyscale = 3.5 * 3.3;
-            sum = 0;// No hollywood
-            var hollywood = resistance / voltage * hollyscale;
-            var total = (sum + hollywood);
-            this.series.add( total );
+            var hollywood = this.resistance / this.voltage * hollyscale;
+            this.series.add(hollywood);
             this.display = this.series.average();
             console.log('does anything need to listen to this?');
         },
@@ -54,10 +37,6 @@ define(function (require) {
 
         coreCountChanged: function(x) {
             this.voltage = x;
-        },
-
-        addParticle: function(p) {
-            this.particles.push(p);
         },
 
         getDisplay: function() {
