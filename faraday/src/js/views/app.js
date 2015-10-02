f69c203aa7a7418c5a7087987a4186aa8bf8b3ad
Patrick Wolfert <patrickrwolfert@gmail.com>
2015-10-02 12:04:00 +0100

    Added the settings button for the app view and made the magnet-and-compass app view extend faraday's.

--- faraday/src/js/views/app.js
+++ faraday/src/js/views/app.js
@@ -8,7 +8,11 @@ define(function(require) {
 
     var Assets = require('assets');
 
+    require('less!common/styles/slider');
     require('less!styles/font-awesome');
+    require('less!styles/app');
+
+    var settingsDialogHtml = require('text!templates/settings-dialog.html');
 
     var FaradayAppView = PixiAppView.extend({
 
@@ -16,7 +20,30 @@ define(function(require) {
 
         simViewConstructors: [
             FaradaySimView
-        ]
+        ],
+
+        events: _.extend({}, PixiAppView.prototype.events, {
+            'slide #needle-spacing-slider' : 'changeNeedleSpacing'
+        }),
+
+        render: function() {
+            PixiAppView.prototype.render.apply(this);
+
+            this.$el.append(settingsDialogHtml);
+
+            this.$('#needle-spacing-slider').noUiSlider({
+                start: 3,
+                range: {
+                    min: 1,
+                    max: 5
+                },
+                step: 1
+            });
+        },
+
+        changeNeedleSpacing: function(event) {
+            
+        }
 
     });
 
