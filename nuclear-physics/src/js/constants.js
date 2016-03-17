af0471900b808b499d16ea887c9154479696362c
Patrick Wolfert <patrickrwolfert@gmail.com>
2016-03-16 17:04:07 -0700

    Simplified the colors given to different isotopes and explained my reasoning behind this decision in the Devations.md file.

--- nuclear-physics/src/js/constants.js
+++ nuclear-physics/src/js/constants.js
@@ -19,75 +19,34 @@ define(function (require) {
     //----------------------------------------------------------------------------
     // Paints and Colors
     //----------------------------------------------------------------------------
+    var defaultColor   = '#ff0';
+    var decayedColor   = '#fff';
+    var alternateColor = '#0f0';
+
+    // Color for the isotope labels used for the nucleus views.
+    Constants.POLONIUM_LABEL_COLOR                  = defaultColor;
+    Constants.LEAD_LABEL_COLOR                      = decayedColor;
+    Constants.CUSTOM_NUCLEUS_LABEL_COLOR            = defaultColor;
+    Constants.CUSTOM_NUCLEUS_POST_DECAY_LABEL_COLOR = decayedColor;
+    Constants.HYDROGEN_3_LABEL_COLOR                = defaultColor;
+    Constants.HELIUM_3_LABEL_COLOR                  = decayedColor;
+    Constants.CARBON_14_LABEL_COLOR                 = defaultColor;
+    Constants.NITROGEN_14_LABEL_COLOR               = decayedColor;
+    Constants.URANIUM_235_LABEL_COLOR               = '#0f0';
+    Constants.URANIUM_236_LABEL_COLOR               = '#f80';
+    Constants.URANIUM_238_LABEL_COLOR               = '#ff0';
+    Constants.URANIUM_239_LABEL_COLOR               = '#fff';
     
-    // Color for the label used for the Polonium nucleus.
-    Constants.POLONIUM_LABEL_COLOR = '#ff0';
-    
-    // Color for label used for the Lead nucleus.
-    Constants.LEAD_LABEL_COLOR = '#000';
-    
-    // Color for label used for the Custom nucleus (pre-decay).
-    Constants.CUSTOM_NUCLEUS_LABEL_COLOR = '#99ffff';
-    
-    // Color for label used for the Decayed Custom nucleus.
-    Constants.CUSTOM_NUCLEUS_POST_DECAY_LABEL_COLOR = '#000';
-    
-    // Color for label used for the Hydrogen 3 nucleus.
-    Constants.HYDROGEN_3_LABEL_COLOR = '#7FFF00';
-    
-    // Color for label used for the Helium 3 nucleus.
-    Constants.HELIUM_3_LABEL_COLOR = '#dfa7fd';
-    
-    // Color for label used for the Carbon 14 nucleus.
-    Constants.CARBON_14_LABEL_COLOR = '#ff0';
-    
-    // Color for label used for the Uranium 235 nucleus.
-    Constants.NITROGEN_14_LABEL_COLOR = '#f80';
-    
-    // Color for label used for the Uranium 235 nucleus.
-    Constants.URANIUM_235_LABEL_COLOR = '#0f0';
-    
-    // Color for label used for the Uranium 236 nucleus.
-    Constants.URANIUM_236_LABEL_COLOR = '#f80';
-    
-    // Color for label used for the Uranium 238 nucleus.
-    Constants.URANIUM_238_LABEL_COLOR = '#ff0';
-    
-    // Color for label used for the Uranium 239 nucleus.
-    Constants.URANIUM_239_LABEL_COLOR = '#fff';
-    
-    // Color for hydrogen when represented as a circle or sphere.
+    // Color for nuclei when represented as a circle or sphere.
     Constants.HYDROGEN_COLOR = '#FFC0DB';
-    
-    // Color for helium when represented as a circle or sphere.
     Constants.HELIUM_COLOR = '#0ff';
-    
-    // Color for carbon when represented as a circle or sphere.
     Constants.CARBON_COLOR = '#C80000';
-    
-    // Color for nitrogen when represented as a circle or sphere.
     Constants.NITROGEN_COLOR = '#0E56C8';
-    
-    // Color for Uranium when represented as a circle or sphere.
     Constants.URANIUM_COLOR = '#969600';
-    
-    // Color for Lead when represented as a circle or sphere.
     Constants.LEAD_COLOR = '#61757E';
-    
-    // Color for Polonium when represented as a circle or sphere.
     Constants.POLONIUM_COLOR = '#f80';
-    
-    // Color for pre-decay custom nucleus when represented as a circle or sphere.
     Constants.CUSTOM_NUCLEUS_PRE_DECAY_COLOR = '#9B612A';
-    
-    // Color for post-decay custom nucleus when represented as a circle or sphere.
     Constants.CUSTOM_NUCLEUS_POST_DECAY_COLOR = '#368237';
-    
-    // Color of the chart background for the alpha decay application.
-    Constants.CHART_BACKGROUND_COLOR = '#F6F2AF';
-    
-    // Color of the reset button that appears on many of the canvases.
-    Constants.CANVAS_RESET_BUTTON_COLOR = '#ff9900';
     
     // Colors for the strata in the Radioactive Dating Game, assumed to go
     // from top to bottom.
