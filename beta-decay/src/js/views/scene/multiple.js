0b96b0d9db233eeabeb099457322a4667231ec73
Patrick Wolfert <patrickrwolfert@gmail.com>
2016-03-22 16:31:24 -0700

    The atom canister (previously "Bucket o' Atoms") is not finished yet, but a lot of the code is here, and I want to save it before I keep going.

--- beta-decay/src/js/views/scene/multiple.js
+++ beta-decay/src/js/views/scene/multiple.js
@@ -11,8 +11,8 @@ define(function(require) {
 
     var ParticleGraphicsGenerator = require('views/particle-graphics-generator');
     var NucleusDecayChart         = require('views/nucleus-decay-chart');
-
-    var NuclearPhysicsSceneView = require('views/scene');
+    var NuclearPhysicsSceneView   = require('views/scene');
+    var AtomCanisterView          = require('views/atom-canister');
 
     /**
      *
@@ -20,7 +20,38 @@ define(function(require) {
     var MultiNucleusBetaDecaySceneView = NuclearPhysicsSceneView.extend({
 
         initialize: function(options) {
+            this.showingLabels = true;
+            this.nucleusViews = [];
+
             NuclearPhysicsSceneView.prototype.initialize.apply(this, arguments);
+
+            this.listenTo(this.simulation.atomicNuclei, 'add', this.nucleusAdded);
+        },
+
+        renderContent: function() {
+            var self = this;
+
+            this.$resetButton = $('<button class="btn btn-lg reset-nuclei-btn">Reset All Nuclei</button>');
+            this.$resetButton.on('click', function() {
+                self.resetNuclei();
+            });
+
+            this.$add10Button = $('<button class="btn add-10-btn">+ 10</button>');
+            this.$add10Button.on('click', function() {
+                self.add10();
+            });
+
+            this.$remove10Button = $('<button class="btn remove-10-btn">- 10</button>');
+            this.$remove10Button.on('click', function() {
+                self.remove10();
+            });
+
+            this.$bucketButtonsWrapper = $('<div class="bucket-btns-wrapper">');
+            this.$bucketButtonsWrapper.append(this.$add10Button);
+            this.$bucketButtonsWrapper.append(this.$remove10Button);
+
+            this.$ui.append(this.$resetButton);
+            this.$ui.append(this.$bucketButtonsWrapper);
         },
 
         initMVT: function() {
@@ -46,8 +77,12 @@ define(function(require) {
         initGraphics: function() {
             NuclearPhysicsSceneView.prototype.initGraphics.apply(this, arguments);
 
+            this.nucleusLayer = new PIXI.Container();
+            this.stage.addChild(this.nucleusLayer);
+
             this.initMVT();
             this.initNucleusDecayChart();
+            this.initAtomCanister();
         },
 
         initNucleusDecayChart: function() {
@@ -59,10 +94,57 @@ define(function(require) {
             this.stage.addChild(this.nucleusDecayChart.displayObject);
         },
 
+        initAtomCanister: function() {
+            this.atomCanisterView = new AtomCanisterView({
+                simulation: this.simulation,
+                width: 160,
+                mvt: this.mvt
+            });
+
+            this.atomCanisterView.displayObject.x = 600;
+            this.atomCanisterView.displayObject.y = 300;
+
+            this.stage.addChild(this.atomCanisterView.displayObject);
+        },
+
         _update: function(time, deltaTime, paused, timeScale) {
             NuclearPhysicsSceneView.prototype._update.apply(this, arguments);
             
             this.nucleusDecayChart.update(time, deltaTime, paused);
+        },
+
+        nucleusAdded: function(nucleus) {
+            var nucleusView = new ExplodingNucleusView({
+                model: this.simulation.atomicNucleus,
+                mvt: this.mvt,
+                showSymbol: this.showingLabels
+            });
+
+            this.nucleusViews.push(nucleusView);
+
+            this.nucleusLayer.addChild(this.nucleusView.displayObject);
+        },
+
+        resetNuclei: function() {
+            console.log('reset nuclei');
+        },
+
+        add10: function() {
+
+        },
+
+        remove10: function() {
+
+        },
+
+        showLabels: function() {
+            this.nucleusView.showLabel();
+            this.showingLabels = true;
+        },
+
+        hideLabels: function() {
+            this.nucleusView.hideLabel();
+            this.showingLabels = false;
         }
 
     });
---
+++ nuclear-physics/src/js/views/atom-canister.js
@@ -0,0 +1,325 @@
+define(function(require) {
+
+    'use strict';
+
+    var _    = require('underscore');
+    var PIXI = require('pixi');
+
+    var PixiView  = require('common/pixi/view');
+    var Rectangle = require('common/math/rectangle');
+    var Vector2   = require('common/math/vector2');
+
+    var NucleusView = require('views/nucleus');
+
+    var Assets = require('assets');
+
+    /**
+     * A visual representation of some kind of object supply.  The
+     *   user creates new objects with this view.  Dragging from 
+     *   the view creates a new object and places it in the scene,
+     *   while dragging an existing object back onto this view
+     *   destroys it.
+     */
+    var AtomCanisterView = PixiView.extend({
+
+        events: {
+            'touchstart      .backgroundLayer': 'dragStart',
+            'mousedown       .backgroundLayer': 'dragStart',
+            'touchmove       .backgroundLayer': 'drag',
+            'mousemove       .backgroundLayer': 'drag',
+            'touchend        .backgroundLayer': 'dragEnd',
+            'mouseup         .backgroundLayer': 'dragEnd',
+            'touchendoutside .backgroundLayer': 'dragEnd',
+            'mouseupoutside  .backgroundLayer': 'dragEnd',
+
+            'mouseover       .backgroundLayer': 'hover',
+            'mouseout        .backgroundLayer': 'unhover'
+        },
+
+        initialize: function(options) {
+            options = _.extend({
+                width: 160,
+                height: undefined,
+
+                labelText: 'Atoms',
+                labelFont: 'bold 16px Helvetica Neue',
+                labelColor: '#000',
+                labelAlpha: 1,
+
+                overlayAlpha: 0.25,
+
+                showHints: true
+            }, options);
+
+            this.mvt = options.mvt;
+            this.simulation = options.simulation;
+            this.dummyLayer = options.dummyLayer;
+
+            this.width = options.width;
+            this.height = options.height;
+
+            this.labelText = options.labelText;
+            this.labelFont = options.labelFont;
+            this.labelColor = options.labelColor;
+            this.labelAlpha = options.labelAlpha;
+
+            this.overlayAlpha = options.overlayAlpha;
+
+            this.showHints = options.showHints;
+            this._showingDragHint = options.showHints;
+
+            // Cached objects
+            this._bounds = new Rectangle();
+
+            this.initGraphics();
+        },
+
+        initGraphics: function() {
+            this.backgroundLayer = new PIXI.Container();
+            this.foregroundLayer = new PIXI.Container();
+            this.hoverLayer = new PIXI.Container();
+
+            this.displayObject.addChild(this.backgroundLayer);
+            this.displayObject.addChild(this.foregroundLayer);
+            this.displayObject.addChild(this.hoverLayer);
+
+            this.initSprites();
+            this.initDecorativeDummyObjects();
+            this.initLabel();
+
+            this.updateMVT(this.mvt);
+        },
+
+        initSprites: function() {
+            var fg = Assets.createSprite(Assets.Images.CANISTER_FG);
+            var bg = Assets.createSprite(Assets.Images.CANISTER_BG);
+            var glow = Assets.createSprite(Assets.Images.CANISTER_GLOW);
+            var drag = Assets.createSprite(Assets.Images.CANISTER_DRAG);
+            var remove = Assets.createSprite(Assets.Images.CANISTER_REMOVE);
+
+            var scale;
+
+            // Calculate the scale
+            if (this.width === undefined && this.height === undefined) {
+                throw 'A width or height must be specified for the canister.';
+            }
+            else if (this.width === undefined) {
+                scale = this.height / fg.height;
+                this.width = this.height * (fg.width / fg.height);
+            }
+            else {
+                scale = this.width / fg.width;
+                this.height = this.width * (fg.height / fg.width);
+            }
+
+            // Apply the scale
+            fg.scale.x = bg.scale.x = glow.scale.x = drag.scale.x = remove.scale.x = scale;
+            fg.scale.y = bg.scale.y = glow.scale.y = drag.scale.y = remove.scale.y = scale;
+
+            // Make overlays invisible by default
+            drag.alpha = 0;
+            remove.alpha = 0;
+
+            // Add everything to their proper layers
+            this.backgroundLayer.addChild(bg);
+            this.foregroundLayer.addChild(fg);
+            this.foregroundLayer.addChild(glow);
+            this.hoverLayer.addChild(drag);
+            this.hoverLayer.addChild(remove);
+
+            // Save references to certain sprites
+            this.glowSprite = glow;
+            this.dragOverlay = drag;
+            this.removeOverlay = remove;
+        },
+
+        initDecorativeDummyObjects: function() {
+            this.decorativeDummyObjects = new PIXI.Container();
+            this.displayObject.addChild(this.decorativeDummyObjects);
+
+            this.drawDecorativeDummyObjects();
+        },
+
+        initLabel: function() {
+            var textSettings = {
+                font: this.labelFont,
+                fill: this.labelColor
+            };
+
+            var label = new PIXI.Text(this.labelText, textSettings);
+            label.anchor.x = 0.5;
+            label.anchor.y = -0.11;
+            label.x = this.width / 2;
+            label.y = this.thickness;
+
+            this.displayObject.addChild(label);
+        },
+
+        drawDecorativeDummyObjects: function() {
+            var windowCenterX = this.width / 2;
+            var windowCenterY = this.height * (108 / 260);
+            var windowRadius  = this.height *  (55 / 260);
+
+            var numberOfDummies = 3;
+            var angleStep = (Math.PI * 2) / numberOfDummies;
+            var startingAngle = Math.random() * Math.PI;
+            var radius = windowRadius * 0.7;
+            var dummy;
+            var vec = new Vector2();
+            
+            for (var n = 0; n < numberOfDummies; n++) {
+                vec.set(radius, 0).rotate(startingAngle + n * angleStep);
+
+                // dummy = this.createDummyObject();
+                // dummy.setPosition(windowCenterX + vec.x, windowCenterY + vec.y);
+
+                // this.decorativeDummyObjects.addChild(dummy.displayObject);
+            }
+        },
+
+        /**
+         * Creates a new object (of whatever this reservoir contains)
+         *   and returns it so it can be added to the scene as a
+         *   dummy object.  Note the dummy object will not be added
+         *   to the simulation until it gets turned into a real
+         *   object after the user drops it.
+         */
+        createDummyObject: function() {
+            var model = new Charge();
+            var view = new ReservoirObjectView({
+                model: model,
+                mvt: this.mvt,
+                interactive: false
+            });
+            return view;
+        },
+
+        destroyObject: function(object) {
+            object.destroy();
+        },
+
+        /**
+         * Creates the actual object based off of the position of the
+         *   dummy object and adds it to the simulation/scene.
+         */
+        createAndAddObject: function(dummyObject) {},
+
+        updateMVT: function(mvt) {
+            this.mvt = mvt;
+
+            this.drawDecorativeDummyObjects();
+        },
+
+        update: function(time, deltaTime, paused) {
+            if (this._showingDragHint)
+                this.oscillateDragOverlayAlpha(time);
+            
+            if (!paused)
+                this.oscillateGlow(time);
+        },
+
+        /**
+         * Makes the drag overlay fade in and out
+         */
+        oscillateDragOverlayAlpha: function(time) {
+            if (!this.hovering)
+                this.dragOverlay.alpha = Math.sin(time) * this.overlayAlpha;
+        },
+
+        oscillateGlow: function(time) {
+            this.glowSprite.alpha = Math.sin(time) * this.overlayAlpha;
+        },
+
+        dragStart: function(data) {
+            this.dragging = true;
+
+            this.dummyObject = this.createDummyObject();
+            this.dummyLayer.addChild(this.dummyObject.displayObject);
+        },
+
+        drag: function(data) {
+            if (this.dragging) {
+                this.dummyObject.setPosition(
+                    data.global.x,
+                    data.global.y
+                );
+            }
+        },
+
+        dragEnd: function(data) {
+            this.dragging = false;
+
+            if (this.dummyObject) {
+                var x = this.dummyObject.displayObject.x;
+                var y = this.dummyObject.displayObject.y;
+
+                if (!this.contains(x, y)) {
+                    // Create a real object and add it to the sim
+                    this.createAndAddObject(this.dummyObject.model);
+                }
+
+                this.dummyObject.removeFrom(this.dummyLayer);
+                this.dummyObject.model.destroy();
+                this.dummyObject = null;
+            }
+        },
+
+        hover: function(event) {
+            this.hovering = true;
+
+            // Only show it if we aren't currently holding down a mouse button
+            if (event.data.originalEvent.button === -1)
+                this.dragOverlay.alpha = this.overlayAlpha;
+        },
+
+        unhover: function(event) {
+            this.hovering = false;
+        },
+
+        getBounds: function() {
+            return this._bounds.set(
+                this.displayObject.x,
+                this.displayObject.y,
+                this.width,
+                this.height
+            );
+        },
+
+        /**
+         * Returns whether or not a circle on the screen at point (x, y)
+         *   with the given radius would overlap with the reservoir.
+         */
+        overlapsCircle: function(x, y, radius) {
+            return this.getBounds().overlapsCircle(x, y, radius);
+        },
+
+        /**
+         * Returns whether or not a point on the screen lies inside the
+         *   reservoir's bounds.
+         */
+        contains: function(x, y) {
+            return this.getBounds().contains(x, y);
+        },
+
+        showDestroyHighlight: function() {
+            this.removeOverlay.alpha = this.overlayAlpha;
+        },
+
+        hideDestroyHighlight: function() {
+            this.removeOverlay.alpha = 0;
+        },
+
+        showDragHint: function() {
+            this._showingDragHint = true;
+        },
+
+        hideDragHint: function() {
+            this._showingDragHint = false;
+            this.dragOverlay.alpha = 0;
+        }
+
+    });
+
+
+    return AtomCanisterView;
+});
