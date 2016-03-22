352a7ab5397777f3c4211832920a6e49b791096b
Amanda Shih <amanda.shih@rice.edu>
2016-03-21 23:20:25 -0500

    ray cap view

--- rutherford-scattering/src/js/views/gun.js
+++ rutherford-scattering/src/js/views/gun.js
@@ -37,19 +37,26 @@ define(function(require) {
          * Initializes everything for rendering graphics
          */
         initGraphics: function() {
-            // this.ray = new PIXI.Graphics();
-
             this.rayGun = Assets.createSprite(Assets.Images.RAY_GUN);
-            this.rayGun.anchor.x = 0.5;
-            this.rayGun.anchor.y = 0.5;
+            this.rayGun.anchor.x = RayGunView.BARREL_CENTER_X;
+            this.rayGun.anchor.y = RayGunView.BARREL_CENTER_Y;
+
+            this.ray = new PIXI.Graphics();
+            this.drawRay();
+
+            this.rayTarget = new PIXI.Graphics();
+            this.rayCap = new PIXI.Graphics();
+            this.drawRayTargetCap();
 
             this.triggerButton = Assets.createSprite(this.getTrigger());
             this.triggerButton.buttonMode = true;
             this.triggerButton.defaultCursor = 'pointer';
             this.triggerButton.anchor.x = 0.5;
-            this.triggerButton.anchor.y = 0.5;
+            this.triggerButton.anchor.y = 0;
 
-            // this.displayObject.addChild(this.ray);
+            this.displayObject.addChild(this.ray);
+            this.displayObject.addChild(this.rayTarget);
+            this.displayObject.addChild(this.rayCap);
             this.displayObject.addChild(this.rayGun);
             this.displayObject.addChild(this.triggerButton);
 
@@ -64,17 +71,22 @@ define(function(require) {
             var center = this.model.get('center');
             this.mvt = mvt;
 
-            var targetWidth = Math.round(this.mvt.modelToViewDeltaX(20));
+            var targetWidth = Math.round(this.mvt.modelToViewDeltaX(RayGunView.WIDTH));
             var scale = targetWidth / this.rayGun.texture.width;
+
+            this.ray.scale.x = scale;
+            this.ray.scale.y = scale;
+
+            this.rayTarget.scale.x = scale;
+            this.rayTarget.scale.y = scale;
+            this.rayCap.scale.x = scale;
+            this.rayCap.scale.y = scale;
 
             this.rayGun.scale.x = scale;
             this.rayGun.scale.y = scale;
 
             this.triggerButton.scale.x = scale;
             this.triggerButton.scale.y = scale;
-            // offset trigger from center
-            this.triggerButton.x = -20 * scale;
-            this.triggerButton.y = 20 * scale;
 
             this.displayObject.x = Math.floor(this.mvt.modelToViewX(center.x));
             this.displayObject.y = Math.floor(this.mvt.modelToViewY(center.y));
@@ -82,8 +94,37 @@ define(function(require) {
             this.update();
         },
 
-        update: function(){
+        update: function() {
             this.updateTrigger();
+        },
+
+        drawRay: function() {
+            this.rayWidth = RayGunView.RAY_WIDTH * this.rayGun.texture.width;
+            this.rayHeight = RayGunView.RAY_HEIGHT * this.rayGun.texture.height;
+
+            this.ray.beginFill(Colors.parseHex('FFF'), 0.25);
+            this.ray.drawRect(-0.5 * this.rayWidth, -1 * this.rayHeight - 0.5 * this.rayGun.texture.height, this.rayWidth, this.rayHeight);
+            this.ray.endFill();
+        },
+
+        drawRayTargetCap: function() {
+            this.rayTargetWidth = 1.4 * this.rayWidth;
+            this.rayTargetHeight = 18;
+            this.rayTargetTop = -1 * this.rayHeight - 0.5 * this.rayGun.texture.height;
+            this.rayTargetLeft = -0.5 * this.rayTargetWidth;
+            this.rayTargetRight = 0.5 * this.rayTargetWidth;
+
+            this.rayTarget.beginFill(Colors.parseHex('3E5FFF'), 1);
+            this.rayTarget.drawRect(this.rayTargetLeft, this.rayTargetTop, this.rayTargetWidth, this.rayTargetHeight);
+            this.rayTarget.endFill();
+
+            this.rayCapHeight = 12;
+            this.rayCap.beginFill(Colors.parseHex('3E5FFF'), 0.5);
+            this.rayCap.moveTo(this.rayTargetLeft, this.rayTargetTop);
+            this.rayCap.lineTo(-0.4 * this.rayTargetWidth, this.rayTargetTop - this.rayCapHeight);
+            this.rayCap.lineTo(0.4 * this.rayTargetWidth, this.rayTargetTop - this.rayCapHeight);
+            this.rayCap.lineTo(this.rayTargetRight, this.rayTargetTop);
+            this.rayCap.endFill();
         },
 
         toggle: function() {
@@ -106,9 +147,10 @@ define(function(require) {
 
         updateTrigger: function() {
             this.triggerButton.texture = Assets.Texture(this.getTrigger());
+            this.ray.visible = this.model.get('on');
         }
 
-    }, {});
+    }, Constants.RayGunView);
 
 
     return RayGunView;
