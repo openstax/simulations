a8432c41015056c69d8b2d57e8aef8a750bd6f3b
Patrick Wolfert <patrickrwolfert@gmail.com>
2016-03-30 11:02:47 -0700

    Gave it the right DELTA_TIME_PER_FRAME for this sim after observing that the nuclei were decaying a lot slower than in the original.

--- radioactive-dating-game/src/js/constants.js
+++ radioactive-dating-game/src/js/constants.js
@@ -6,6 +6,9 @@ define(function (require) {
 
     var Constants = require('nuclear-physics/constants'); 
 
+    Constants.FRAME_RATE = 25;
+    Constants.DELTA_TIME_PER_FRAME = 40;
+
     // Preferred distance between nucleus centers when placing them on the canvas.
     Constants.PREFERRED_INTER_NUCLEUS_DISTANCE = 15;  // In femtometers.
     // Minimum distance between the center of a nucleus and a wall or other obstacle.
