a3152a1310d7364ea2e49d8f8292cd32db09d33f
Patrick Wolfert <patrickrwolfert@gmail.com>
2016-05-06 08:10:20 -0700

    Some graphics drawing for the energy level

--- lasers/src/js/views/energy-level.js
+++ lasers/src/js/views/energy-level.js
@@ -36,6 +36,7 @@ define(function(require) {
         initialize: function(options) {
             options = _.extend({
                 levelNumber: 0,
+                atomRadius: 6,
                 wavelengthChangeEnabled: true,
                 lifetimeChangeEnabled: true
             }, options);
@@ -47,6 +48,7 @@ define(function(require) {
             this.groundStateEnergy = options.groundStateEnergy;
             this.minY = options.minY;
             this.maxY = options.maxY;
+            this.atomRadius = options.atomRadius;
             this.levelNumber = options.levelNumber;
             this.wavelengthChangeEnabled = options.wavelengthChangeEnabled;
             this.lifetimeChangeEnabled = options.lifetimeChangeEnabled;
@@ -69,19 +71,32 @@ define(function(require) {
 
             this.wavelengthColorGraphics = new PIXI.Graphics();
 
+            this.atomSprite = Assets.createSprite(Assets.Images.SPHERE);
+            this.atomSprite.anchor.x = 0.5;
+            this.atomSprite.anchor.y = 0.5;
+            this.atomSprite.scale.x = this.atomSprite.scale.y = ((this.atomRadius * 2) / this.atomSprite.texture.width);
+            this.atomSprite.x = this.width - 80;
 
             this.displayObject.addChild(this.wavelengthColorGraphics);
+            this.displayObject.addChild(this.atomSprite);
             this.displayObject.addChild(this.dragHandle);
         },
 
         drawWavelengthColor: function() {
             var graphics = this.wavelengthColorGraphics;
             var color = Colors.parseHex(this.getColor());
+            var radius = this.getRadius();
+            var atomCenterX = this.atomSprite.x;
 
             graphics.clear();
+
+            graphics.beginFill(color, 1);
+            graphics.drawCircle(atomCenterX, 0, radius);
+            graphics.endFill();
+
             graphics.lineStyle(2, color, 1);
             graphics.moveTo(0, 0);
-            graphics.lineTo(this.width, 0);
+            graphics.lineTo(atomCenterX, 0);
         },
 
         getColor: function() {
@@ -91,6 +106,13 @@ define(function(require) {
 
             var deltaEnergy = energy - this.groundStateEnergy;
             return WavelengthColors.nmToHex(PhysicsUtil.energyToWavelength(deltaEnergy));
+        },
+
+        getRadius: function() {
+            if (this.model.getEnergyLevel() === this.groundStateEnergy)
+                return this.atomRadius + 3;
+            else
+                return this.atomRadius + 10;
         },
 
         setMinY: function(minY) {
