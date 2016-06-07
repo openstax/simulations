f3c98a69627a8ee9b68d6f11bd37e28d3d0c9e5e
Patrick Wolfert <patrickrwolfert@gmail.com>
2016-06-07 12:49:37 -0700

    The experiment mode has the question mark overlay.

--- hydrogen-atom/src/js/views/atomic-model/experiment.js
+++ hydrogen-atom/src/js/views/atomic-model/experiment.js
@@ -20,8 +20,44 @@ define(function(require) {
             AtomicModelView.prototype.initialize.apply(this, arguments);
         },
 
-        update: function(time, deltaTime, paused) {
-            AtomicModelView.prototype.update.apply(this, arguments);
+        /**
+         * Initializes everything for rendering graphics
+         */
+        initGraphics: function() {
+            AtomicModelView.prototype.initGraphics.apply(this, arguments);
+
+            this.graphics = new PIXI.Graphics();
+
+            this.questionMark = new PIXI.Text('?', {
+                font: '90px Helvetica Neue',
+                fill: '#fff'
+            });
+            this.questionMark.resolution = this.getResolution();
+            this.questionMark.anchor.x = 0.5;
+            this.questionMark.anchor.y = 0.5;
+
+            this.displayObject.addChild(this.graphics);
+            this.displayObject.addChild(this.questionMark);
+        },
+
+        /**
+         * Updates the model-view-transform and anything that relies on it.
+         */
+        updateMVT: function(mvt) {
+            AtomicModelView.prototype.updateMVT.apply(this, arguments);
+
+            var atomPosition = this.getViewPosition();
+            this.graphics.x = atomPosition.x;
+            this.graphics.y = atomPosition.y;
+            this.questionMark.x = atomPosition.x;
+            this.questionMark.y = atomPosition.y;
+
+            var size = this.mvt.modelToViewDeltaX(130);
+            this.graphics.clear();
+            this.graphics.lineStyle(1, 0xFFFFFF, 1);
+            this.graphics.beginFill(0x233C77, 1);
+            this.graphics.drawRect(-size / 2, -size / 2, size, size);
+            this.graphics.endFill();
         }
 
     });
